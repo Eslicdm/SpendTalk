937c38ad5eef245f0c5d39a115db10903de78ddd
Eslicdm <eslicdm1@gmail.com>
2022-03-14 10:47:36 -0300

    Tutorial Sample

---
+++ app/src/main/java/com/eslirodrigues/spendtalk/MessagesActivity.kt
@@ -0,0 +1,52 @@
+package com.eslirodrigues.spendtalk
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.compose.ui.messages.MessagesScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.compose.ui.theme.StreamShapes
+
+class MessagesActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 1 - Load the ID of the selected channel
+        val channelId = intent.getStringExtra(KEY_CHANNEL_ID)
+
+        if (channelId == null) {
+            finish()
+            return
+        }
+
+        // 2 - Add the MessagesScreen to your UI
+        setContent {
+            ChatTheme(
+
+            ) {
+                MessagesScreen(
+                    channelId = channelId,
+                    messageLimit = 30,
+                    onBackPressed = { finish() }
+                )
+            }
+        }
+    }
+
+    // 3 - Create an intent to start this Activity, with a given channelId
+    companion object {
+        private const val KEY_CHANNEL_ID = "channelId"
+
+        fun getIntent(context: Context, channelId: String): Intent {
+            return Intent(context, MessagesActivity::class.java).apply {
+                putExtra(KEY_CHANNEL_ID, channelId)
+            }
+        }
+    }
+}
