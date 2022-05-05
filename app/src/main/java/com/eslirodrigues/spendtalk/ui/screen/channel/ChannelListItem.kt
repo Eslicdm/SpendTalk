30b0c6e8c524e5a6aa26567b8061b26fe890a96f
Eslicdm <eslicdm1@gmail.com>
2022-05-05 10:19:22 -0300

    Adding image in ChannelListItem

--- app/src/main/java/com/eslirodrigues/spendtalk/ui/screen/channel/ChannelListItem.kt
+++ app/src/main/java/com/eslirodrigues/spendtalk/ui/screen/channel/ChannelListItem.kt
@@ -1,41 +1,99 @@
 package com.eslirodrigues.spendtalk.ui.screen.channel
 
-import androidx.compose.foundation.Canvas
+import android.graphics.BitmapFactory
+import android.util.Base64
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.eslirodrigues.spendtalk.R
+import com.eslirodrigues.spendtalk.core.state.UserState
 import com.eslirodrigues.spendtalk.data.model.Channel
 import com.eslirodrigues.spendtalk.ui.screen.destinations.MessageScreenDestination
+import com.eslirodrigues.spendtalk.ui.theme.LightGreen
+import com.eslirodrigues.spendtalk.ui.viewmodel.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
-fun ChannelListItem(navigator: DestinationsNavigator, channel: Channel) {
+fun ChannelListItem(navigator: DestinationsNavigator, channel: Channel, userViewModel: UserViewModel = viewModel()) {
 
     val currentUserEmail = FirebaseAuth.getInstance().currentUser?.email
 
-    Card(Modifier.fillMaxWidth().height(60.dp).padding(top = 7.dp)) {
+    val emailName = if (channel.friendEmail == currentUserEmail) channel.creatorEmail else channel.friendEmail
+
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(top = 7.dp)
+    ) {
         Row(
-            modifier = Modifier.padding(start = 20.dp)
+            modifier = Modifier
+                .padding(start = 6.dp)
                 .clickable {
                     navigator.navigate(MessageScreenDestination(channel))
                 },
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
         ) {
+            Card(
+                Modifier
+                    .size(45.dp)
+                    .clip(CircleShape)
+                    .border(width = 2.dp, color = LightGreen, shape = CircleShape)
+            ) {
+                when(val userResult = userViewModel.userState.value) {
+                    is UserState.Success -> {
+                        val users = userResult.data.filter { it.email == emailName  }
+                        val imageBitmap = users.firstOrNull()?.image
+                        if (!imageBitmap.isNullOrEmpty()) {
+                            val imageBytes = Base64.decode(imageBitmap, 0)
+                            val image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
 
-            Text(text = if (channel.friendEmail == currentUserEmail) channel.creatorEmail else channel.friendEmail)
-            if (channel.hasMessage) {
-                Canvas(modifier = Modifier.size(42.dp).padding(end = 30.dp), onDraw = {
-                    drawCircle(color = Color.Red)
-                })
+                            Image(
+                                bitmap = image.asImageBitmap(),
+                                contentScale = ContentScale.Crop,
+                                contentDescription = stringResource(id = R.string.user_profile),
+                            )
+                        } else {
+                            Icon(Icons.Default.AccountCircle, contentDescription = stringResource(id = R.string.user_profile), modifier = Modifier.fillMaxSize())
+                        }
+                    }
+                    is UserState.Loading -> {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                        }
+                    }
+                    else -> {
+                        Icon(Icons.Default.AccountCircle, contentDescription = stringResource(id = R.string.user_profile), modifier = Modifier.fillMaxSize())
+                    }
+                }
             }
+            Text(text = emailName, modifier = Modifier.padding(start = 8.dp))
+//            if (true) {
+//                Canvas(
+//                    modifier = Modifier.size(22.dp),
+//                    onDraw = {
+//                        drawCircle(color = Color.Red)
+//                    }
+//                )
+//            }
         }
     }
 }
